e326d241e9f5c61b31f51659015ef21383bb1eff
Andrew Ivanov <andrew11.12.97@mail.ru>
2024-02-29 20:11:45 +0300

    update payment views

--- myshop/payment/views.py
+++ myshop/payment/views.py
@@ -26,3 +26,25 @@ def payment_process(request):
                         'line_items': []
                         }
         
+        for item in order.items.all():
+            session_data['line_items'].append({
+                'price_data': {
+                    'unit_amount': int(item.price * Decimal('100')),
+                    'currence': 'usd',
+                    'product_data': {'name': item.procduct.name, 
+                                     },
+                },
+                'quantity': item.quantity,
+            })
+        # Создание сеанса оформления платежа
+        session = stripe.checkout.Session.create(**session_data)
+        # Перенаправить к платежной платформе stripe
+        return redirect(session.url, code = 303)
+    else:
+        return render(request, 'payment/process.html', locals())
+        
+def payment_completed(request):
+    return render(request, 'payment/completed.html')
+
+def payment_canceled(request):
+    return render(request, 'payment/canceled.html')

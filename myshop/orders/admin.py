271143bb9a2e8569b35aba14356f9061d10bb079
Andrew Ivanov <andrew11.12.97@mail.ru>
2024-03-21 18:38:23 +0300

    create pdf template

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -7,6 +7,11 @@ from django.urls import reverse
 
 
 # Register your models here.
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+order_pdf.short_description = 'Invoice'
+
 def order_detail(obj):
     url = reverse('orders:admin_order_detail', args=[obj.id])
     return mark_safe(f'<a href="{url}">View</a>')
